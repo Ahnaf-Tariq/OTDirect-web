1e24488d2d3cde1fad69bd0044b8604284610dd3
Ahnaf-Hamid <ahnafhamid7@gmail.com>
2025-06-13 10:57:45 -0700

    start of day2

--- frontend/src/components/navbar/Navbar.jsx
+++ frontend/src/components/navbar/Navbar.jsx
@@ -28,7 +28,7 @@ const Navbar = () => {
                     {links.subMenu && <RiArrowDropDownLine />}
                   </NavLink>
                   {links.subMenu && (
-                    <div className="absolute hidden group-hover:block border rounded-md shadow-lg w-56 bg-white text-gray-500 left-[-20px] mt-2 p-2">
+                    <div className="absolute hidden group-hover:block border rounded-md shadow-lg w-56 bg-white text-gray-500 left-[-20px]  p-2">
                       {links.subLinks.map((sublink, i) => (
                         <li
                           key={i}
@@ -41,29 +41,6 @@ const Navbar = () => {
                   )}
                 </div>
               ))}
-              {/* <NavLink to='/'>Home</NavLink>
-              <div className="relative group">
-                <li className="flex items-center gap-1">Properties <RiArrowDropDownLine /></li>
-                <div className="absolute hidden group-hover:block border rounded-md shadow-lg w-52 bg-white text-gray-500 left-[-20px] mt-2 p-2">
-                  <li className="p-[4px] hover:text-orange-800 text-base">All Properties</li>
-                  <li className="p-[4px] hover:text-orange-800 text-base">Featured Properties</li>
-                  <li className="p-[4px] hover:text-orange-800 text-base">Most Viewed Properties</li>
-                  <li className="p-[4px] hover:text-orange-800 text-base">Nearby Cities Properties</li>
-                  <li className="p-[4px] hover:text-orange-800 text-base">Most Favorite Properties</li>
-                </div>
-              </div>
-              <div className="relative group">
-                <li className="flex items-center gap-1">Pages <RiArrowDropDownLine /></li>
-                <div className="absolute hidden group-hover:block border rounded-md shadow-lg w-44 bg-white text-gray-500 left-[-20px] mt-2 p-2">
-                  <li className="p-[4px] hover:text-orange-800 text-base">Subscription Plan</li>
-                  <li className="p-[4px] hover:text-orange-800 text-base">Trends</li>
-                  <li className="p-[4px] hover:text-orange-800 text-base">Area Converter</li>
-                  <li className="p-[4px] hover:text-orange-800 text-base">Terms & Conditions</li>
-                  <li className="p-[4px] hover:text-orange-800 text-base">Privacy Policy</li>
-                </div>
-              </div>
-              <NavLink to='contact-us'>Contact Us</NavLink>
-              <NavLink to='about-us'>About Us</NavLink> */}
             </ul>
             <button className="rounded h-10 px-2 font-semibold text-orange-800 bg-white cursor-pointer">
               <span className="text-xl">+</span> Add Property
