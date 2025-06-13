549c3fcd4d959d4ce7db72fc4d9b95f8304978f8
Ahnaf-Hamid <ahnafhamid7@gmail.com>
2025-06-13 11:50:27 -0700

    navbar fully completed with responsiveness

--- frontend/src/components/navbar/Navbar.jsx
+++ frontend/src/components/navbar/Navbar.jsx
@@ -8,6 +8,7 @@ import navLinks from "./navlinks";
 
 const Navbar = () => {
   const [sideBar, setSideBar] = useState(false);
+  const [sideBarLinks, setSideBarLinks] = useState(null);
   return (
     <>
       <nav className="bg-orange-800 h-20">
@@ -22,16 +23,19 @@ const Navbar = () => {
           <div className="hidden lg:flex items-center gap-6 text-white">
             <ul className="flex items-center gap-8 text-lg font-semibold cursor-pointer">
               {navLinks.map((links, index) => (
-                <div key={index} className="relative group">
-                  <NavLink className="flex items-center gap-1">
+                <div key={index}>
+                  <div
+                    onClick={() => setSideBarLinks(!sideBarLinks)}
+                    className="flex items-center gap-1"
+                  >
                     {links.title}
                     {links.subMenu && <RiArrowDropDownLine />}
-                  </NavLink>
-                  {links.subMenu && (
-                    <div className="absolute hidden group-hover:block border rounded-md shadow-lg w-56 bg-white text-gray-500 left-[-20px]  p-2">
-                      {links.subLinks.map((sublink, i) => (
+                  </div>
+                  {links.subMenu && sideBarLinks === true && (
+                    <div className="w-56 text-gray-500 left-[-20px] p-2">
+                      {links.subLinks.map((sublink, ind) => (
                         <li
-                          key={i}
+                          key={ind}
                           className="p-[4px] hover:text-orange-800 text-base"
                         >
                           {sublink.title}
@@ -61,7 +65,7 @@ const Navbar = () => {
       {/* ********************** Sidebar *************************** */}
       {sideBar && (
         <div
-          className={`fixed top-0 left-0 h-full w-96 bg-orange-800 text-white z-50 transform transition-transform duration-300 `}
+          className={`fixed top-0 left-0 h-full w-60 sm:w-96 bg-orange-800 text-white z-10`}
         >
           <div className="flex justify-between items-center px-6 py-4 border-b border-orange-300">
             <img
@@ -75,11 +79,28 @@ const Navbar = () => {
             />
           </div>
           <ul className="flex flex-col gap-4 p-6 text-lg font-semibold cursor-pointer">
-            <li>Home</li>
-            <li>Properties</li>
-            <li>Pages</li>
-            <li>Contact Us</li>
-            <li>About Us</li>
+            {navLinks.map((links, index) => (
+              <div key={index}>
+                <div
+                  onClick={() =>
+                    setSideBarLinks(
+                      sideBarLinks === links.title ? null : links.title
+                    )
+                  }
+                  className="flex items-center gap-1"
+                >
+                  {links.title}
+                  {links.subMenu && <RiArrowDropDownLine />}
+                </div>
+                {links.subMenu && sideBarLinks === links.title && (
+                  <div className="ml-4 mt-2 p-2 font-normal">
+                    {links.subLinks.map((sublinks, ind) => (
+                      <li key={ind}>{sublinks.title}</li>
+                    ))}
+                  </div>
+                )}
+              </div>
+            ))}
           </ul>
           <div className="px-6 flex gap-2">
             <button className="rounded h-10 px-2 font-semibold text-orange-800 bg-white cursor-pointer">
