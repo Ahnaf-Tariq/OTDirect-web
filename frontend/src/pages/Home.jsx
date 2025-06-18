b8c543909f5727ec81543e47f5df84fe06f0163e
Ahnaf-Hamid <ahnafhamid7@gmail.com>
2025-06-18 14:39:25 -0700

    most viewed property setup

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import FeaturedListings from "../components/featured listings/FeaturedListings";
 import Hero from "../components/hero/Hero";
+import ViewedProperties from "../components/viewed properties/ViewedProperties";
 
 const Home = () => {
   return (
     <div>
       <Hero />
       <FeaturedListings />
+      <ViewedProperties />
     </div>
   );
 };
