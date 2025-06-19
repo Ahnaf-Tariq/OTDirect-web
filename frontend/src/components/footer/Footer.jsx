6713c71c03035379a04662dc4b11ee20ae66017d
Ahnaf-Hamid <ahnafhamid7@gmail.com>
2025-06-19 10:34:50 -0700

    l

--- frontend/src/components/footer/Footer.jsx
+++ frontend/src/components/footer/Footer.jsx
@@ -4,7 +4,7 @@ import { IoMdMail } from "react-icons/io";
 const Footer = () => {
   return (
     <div className="mt-10 bg-orange-800">
-      <div className="max-w-[1450px] mx-auto py-10 flex justify-between border-b-2 border-b-gray-300">
+      <div className="max-w-[1450px] mx-auto py-10 flex justify-between border-b-2 border-b-gray-100">
         <div className="flex flex-col items-center gap-6">
           <img
             onClick={() => navigate("/")}
