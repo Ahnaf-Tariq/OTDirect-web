611696b7e76296c50200ddba9e63d322b42fb08b
Ahnaf-Hamid <ahnafhamid7@gmail.com>
2025-06-21 16:14:44 -0700

    about page done with responsiveness

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,22 +1,15 @@
-import React from 'react'
-import TitleImage from '../components/TitleImage'
+import React from "react";
+import TitleImage from "../components/TitleImage";
 
 const About = () => {
   return (
     <>
-      <TitleImage title={'ABOUT'}/>
-      <div className='max-w-[1450px] mx-auto flex justify-between gap-3'>
-        <div className='w-1/2'>
-          <h1 className='text-xl font-semibold'>font</h1>
-          <input type="text" className='w-full' placeholder='enter' />
-        </div>
-        <div className='w-1/2'>
-          <h1 className='text-xl font-semibold'>font</h1>
-          <input type="text" className='w-full' placeholder='enter' />
-        </div>
+      <TitleImage title={"About Us"} />
+      <div className="p-24">
+
       </div>
     </>
-  )
-}
+  );
+};
 
-export default About
+export default About;
