ac581d8ac942d6f12e26d0b2908a0f761136dbec
Ahnaf-Hamid <ahnafhamid7@gmail.com>
2025-06-26 13:07:05 -0700

    most favourite property page done with responsiveness

--- frontend/src/pages/properties pages/MostViewed.jsx
+++ frontend/src/pages/properties pages/MostViewed.jsx
@@ -6,11 +6,11 @@ const MostViewed = () => {
   return (
     <div>
       <div>
-      <TitleImage title={"Most Viewed Properties"} />
-      <div>
-        <Properties24 />
+        <TitleImage title={"Most Viewed Properties"} />
+        <div>
+          <Properties24 />
+        </div>
       </div>
-    </div>
     </div>
   );
 };
