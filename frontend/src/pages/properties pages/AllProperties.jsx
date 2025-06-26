6bf3b13db3195608389baa6a0401f79900cc5d0c
Ahnaf-Hamid <ahnafhamid7@gmail.com>
2025-06-26 12:43:40 -0700

    logic added in allproperty

--- frontend/src/pages/properties pages/AllProperties.jsx
+++ frontend/src/pages/properties pages/AllProperties.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { FaHouse, FaBuilding, FaIndustry } from "react-icons/fa6";
 import { FaSearch, FaFilter } from "react-icons/fa";
 import { RiArrowDropDownLine } from "react-icons/ri";
@@ -6,18 +6,40 @@ import FeaturedListings from "../../components/featured listings/FeaturedListing
 import Properties24 from "../../components/24 properties/24Properties";
 
 const AllProperties = () => {
+  const [colorFilter, setColorFilter] = useState("");
   return (
     <div className="px-2 py-10">
       <div className="pt-20 mb-14 flex justify-center overflow-x-hidden">
         <div className="w-full max-w-screen-sm">
           <div className="flex gap-4 mt-4">
-            <p className="flex items-center gap-[2px] sm:gap-2 cursor-pointer text-base md:text-lg">
+            <p
+              onClick={() => setColorFilter("Residential")}
+              className={`${
+                colorFilter === "Residential"
+                  ? "text-[#7A1233] border-b-3 border-[#7A1233] pb-1"
+                  : ""
+              } flex items-center gap-[2px] sm:gap-2 cursor-pointer text-base md:text-lg`}
+            >
               <FaHouse /> Residential
             </p>
-            <p className="flex items-center gap-[2px] sm:gap-2 cursor-pointer text-base md:text-lg">
+            <p
+              onClick={() => setColorFilter("Commercial")}
+              className={`${
+                colorFilter === "Commercial"
+                  ? "text-[#7A1233] border-b-3 border-[#7A1233] pb-1"
+                  : ""
+              } flex items-center gap-[2px] sm:gap-2 cursor-pointer text-base md:text-lg`}
+            >
               <FaBuilding /> Commercial
             </p>
-            <p className="flex items-center gap-[2px] sm:gap-2 cursor-pointer text-base md:text-lg">
+            <p
+              onClick={() => setColorFilter("Industrial")}
+              className={`${
+                colorFilter === "Industrial"
+                  ? "text-[#7A1233] border-b-3 border-[#7A1233] pb-1"
+                  : ""
+              } flex items-center gap-[2px] sm:gap-2 cursor-pointer text-base md:text-lg`}
+            >
               <FaIndustry /> Industrial
             </p>
           </div>
