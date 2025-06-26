1ee7209b7821994277da9be0a100a5ac7f38fa73
Ahnaf-Hamid <ahnafhamid7@gmail.com>
2025-06-26 16:09:43 -0700

    pages routes setup

---
+++ frontend/src/pages/pages/SubscriptionPlan.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SubscriptionPlan = () => {
+  return (
+    <div>
+        
+    </div>
+  )
+}
+
+export default SubscriptionPlan
---
+++ frontend/src/pages/pages/Terms&Conditions.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const TermsConditions = () => {
+  return (
+    <div></div>
+  )
+}
+
+export default TermsConditions
