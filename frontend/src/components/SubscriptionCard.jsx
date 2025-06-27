47c1c0ccafc6acf21d97beac9cb37a404ba00923
Ahnaf-Hamid <ahnafhamid7@gmail.com>
2025-06-27 16:57:15 -0700

    subscription page completed with responsiveness

--- frontend/src/components/SubscriptionCard.jsx
+++ frontend/src/components/SubscriptionCard.jsx
@@ -1,11 +1,29 @@
-import React from 'react'
+import React from "react";
 
-const SubscriptionCard = ({ planType, price, propertyLimit}) => {
+const SubscriptionCard = ({ planType, price, billingCycle, propertyLimit }) => {
   return (
-    <div>
-        
-    </div>
-  )
-}
+    <div className="flex flex-col gap-9 rounded-xl shadow-xl p-6 w-full">
+      <div className="bg-gray-100 text-sm px-3 py-1 rounded-full self-start font-medium">
+        {planType}
+      </div>
+      <div className="flex flex-col gap-4">
+        <div className="text-gray-500 text-sm">{billingCycle}ly Price</div>
 
-export default SubscriptionCard
+        <div className="text-3xl font-bold">
+          {price === 'Free' ? `${price}` : `Pkr ${price}` }
+          <span className="text-gray-500 text-base font-normal">/{billingCycle}</span>
+        </div>
+      </div>
+
+      <div className="flex items-center font-medium gap-1">
+        ✅ <span className="text-black">Property limit: {propertyLimit}</span>
+      </div>
+
+      <button className="w-full bg-[#7A1233] text-white font-semibold py-2 rounded-lg hover:bg-[#600f2a] cursor-pointer">
+        Subscribe
+      </button>
+    </div>
+  );
+};
+
+export default SubscriptionCard;
--- frontend/src/pages/pages/SubscriptionPlan.jsx
+++ frontend/src/pages/pages/SubscriptionPlan.jsx
@@ -7,12 +7,12 @@ const SubscriptionPlan = () => {
   return (
     <div>
       <TitleImage title={"Subscription Plan"} />
-      <div className="max-w-[1450px] mx-auto">
+      <div className="max-w-[1450px] mx-auto px-4">
         <div className="flex justify-center my-10">
-          <div className="inline-flex border border-[#7A1233] rounded overflow-hidden">
+          <div className="inline-flex border border-[#7A1233] rounded-lg overflow-hidden">
             <p
               onClick={() => setPlan("Monthly")}
-              className={`px-7 py-3 font-semibold text-xl cursor-pointer ${
+              className={`px-5 sm:px-7 py-2 sm:py-3 font-semibold text-base sm:text-xl cursor-pointer ${
                 plan === "Monthly"
                   ? "bg-[#7A1233] text-white"
                   : "text-black bg-white"
@@ -22,7 +22,7 @@ const SubscriptionPlan = () => {
             </p>
             <p
               onClick={() => setPlan("Yearly")}
-              className={`px-7 py-3 font-semibold text-xl cursor-pointer ${
+              className={`px-5 sm:px-7 py-2 sm:py-3 font-semibold text-base sm:text-xl cursor-pointer ${
                 plan === "Yearly"
                   ? "bg-[#7A1233] text-white"
                   : "text-black bg-white"
@@ -32,11 +32,25 @@ const SubscriptionPlan = () => {
             </p>
           </div>
         </div>
-        <h1 className="text-2xl font-semibold">
+        <h1 className="text-xl sm:text-2xl font-semibold">
           Choose a Plan that's right for you
         </h1>
-        <div>
-          <SubscriptionCard />
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 py-4 pb-14 ">
+          {plan === "Monthly" ? (
+            <>
+              <SubscriptionCard planType={"Basic"} billingCycle={'Month'} price={"Free"} propertyLimit={3} />
+              <SubscriptionCard planType={"Budget"} billingCycle={'Month'} price={"200"} propertyLimit={5} />
+              <SubscriptionCard planType={"Standard"} billingCycle={'Month'} price={"1000"} propertyLimit={10} />
+              <SubscriptionCard planType={"Premium"} billingCycle={'Month'} price={"3000"} propertyLimit={20} />
+            </>
+          ) : (
+            <>
+              <SubscriptionCard planType={"Basic"} billingCycle={'Year'} price={"Free"} propertyLimit={3} />
+              <SubscriptionCard planType={"Budget"} billingCycle={'Year'} price={"2200"} propertyLimit={5} />
+              <SubscriptionCard planType={"Standard"} billingCycle={'Year'} price={"9000"} propertyLimit={10} />
+              <SubscriptionCard planType={"Premium"} billingCycle={'Year'} price={"27000"} propertyLimit={20} />
+            </>
+          )}
         </div>
       </div>
     </div>
