ad2dfbf7bab79802443a4a01dc7217f3440ebcd2
Ahnaf-Hamid <ahnafhamid7@gmail.com>
2025-07-02 14:04:36 -0700

    Signup user controller func made

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -1,6 +1,6 @@
 import userModel from "../model/userModel.js";
 import validator from "validator";
-import bcrypt from "bcrypt";
+import bcrypt, { genSalt } from "bcrypt";
 import jwt from "jsonwebtoken";
 
 const createToken = (id) => {
@@ -12,23 +12,26 @@ const loginUser = async (req, res) => {
     const { email, phone, password } = req.body;
 
     if (!password || (!email && !phone)) {
-      res.json({ success: false, msg: "Missing Required Fields" });
+      return res.json({ success: false, msg: "Missing Required Fields" });
     }
 
     const user = email
       ? await userModel.findOne({ email })
       : await userModel.findOne({ phone });
 
+    // user exist or not
     if (!user) {
-      res.json({ success: false, msg: "User does not exist" });
+      return res.json({ success: false, msg: "User does not exist" });
     }
 
+    // compare password
     const comparePass = await bcrypt.compare(password, user.password);
 
     if (!comparePass) {
-      res.json({ success: false, msg: "Passwords dont match" });
+      return res.json({ success: false, msg: "Passwords dont match" });
     }
 
+    // token sign
     const token = createToken(user._id);
     res.json({ success: false, token });
   } catch (error) {
@@ -39,8 +42,59 @@ const loginUser = async (req, res) => {
 
 const registerUser = async (req, res) => {
   try {
+    const { fullName, email, phone, password, location, address } = req.body;
+
+    if (!fullName || !password || !location || !address || (!email && !phone)) {
+      return res.json({ success: false, msg: "Missing Required Fields" });
+    }
+
+    // email exists
+    if (email) {
+      const emailExist = await userModel.findOne({ email });
+      if (emailExist) {
+        return res.json({ success: false, msg: "Email already in use" });
+      }
+      if (!validator.isEmail(email)) {
+        return res.json({ success: false, msg: "Invalid Email" });
+      }
+    }
+
+    // phone exists
+    if (phone) {
+      const phoneExist = await userModel.findOne({ phone });
+      if (phoneExist) {
+        return res.json({ success: false, msg: "Phone number already in use" });
+      }
+    }
+
+    // strong password
+    if (password.length < 8) {
+      return res.json({
+        success: false,
+        msg: "Please enter a strong password",
+      });
+    }
+
+    // hashing password
+    const salt = await bcrypt.genSalt(10);
+    const hashPass = await bcrypt.hash(password, salt);
+
+    // model create
+    const user = await userModel.create({
+      fullName,
+      email,
+      phone,
+      password: hashPass,
+      location,
+      address,
+    });
+
+    // token sign
+    const token = createToken(user._id);
+    res.json({ success: false, token });
   } catch (error) {
     console.log(error);
+    res.json({ success: false, msg: error.message });
   }
 };
 
--- backend/model/userModel.js
+++ backend/model/userModel.js
@@ -1,9 +1,9 @@
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
-  FullName: { type: String, required: true },
-  phone: { type: Number, required: true },
-  email: { type: String, required: true, unique: true },
+  fullName: { type: String, required: true },
+  phone: { type: Number, required: false },
+  email: { type: String, required: false, unique: true },
   password: { type: String, required: true },
   location: { type: String, required: true },
   address: { type: String, required: true },
