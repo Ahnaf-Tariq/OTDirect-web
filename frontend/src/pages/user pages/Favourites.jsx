e26122f878033bb608961caa216dd9153ecf8e7f
Ahnaf-Hamid <ahnafhamid7@gmail.com>
2025-07-05 18:22:45 -0700

    favourite user page made

--- frontend/src/pages/user pages/Favourites.jsx
+++ frontend/src/pages/user pages/Favourites.jsx
@@ -34,7 +34,22 @@ const Favourites = () => {
               <SidebarLogo />
             </div>
           )}
-          <div className="px-4 w-full"></div>
+          <div className="px-4 w-full">
+            <h1 className="text-2xl font-semibold p-6">Favourites</h1>
+            <div className="flex flex-col justify-center items-center gap-1 mt-18 px-2">
+              <img
+                className="size-[130px] sm:size-[180px] mb-4"
+                src="https://otdirect.vercel.app/_next/static/media/no_data_found_illustrator.c5c383a7.svg"
+                alt=""
+              />
+              <h1 className="text-2xl text-[#7A1233] font-semibold">
+                No Data Found
+              </h1>
+              <p>
+                We're sorry what you were looking for. please try another way
+              </p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
